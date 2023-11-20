e139927bdcdd002cfa2bcff3689ebd58e38f684b
Trevor Harada <trevor.harada2015@gmail.com>
2023-11-19 18:58:10 -0600

    .

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+// if year % 4 === 0 it is a leap year, but for years that are century years, century years % 400 === 0, for it to be a leap year
+    if (year % 100 === 0 && year % 400 === 0) {
+        return true;
+    } else if (year % 100 != 0 && year % 4 === 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line

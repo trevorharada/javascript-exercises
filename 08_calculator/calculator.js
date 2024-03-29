18e59b13f9935f766de772b4e13530317eefacb1
Trevor Harada <trevor.harada2015@gmail.com>
2024-03-29 06:17:10 -0500

    .

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((sum, current) => sum * current);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let sum = 1;
+  if (a === 0) {
+    return sum;
+  } else {
+    for (let i = a; i > 1; i--) {
+      sum *= i
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line

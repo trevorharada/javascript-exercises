4faecb08112fde93ea427327c85f98347177e736
Trevor Harada <trevor.harada2015@gmail.com>
2024-03-30 07:12:38 -0500

    .

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (x) {
+    let a = x.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let arr = a.split('').reverse().join('');
+    if (arr == a) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+// find a way to make everything lowercase
+//find a way to remove non words
+//find a way to join the string into one word, no spaces 
+//find a way to compare new string to original string
+
 
 // Do not edit below this line
 module.exports = palindromes;

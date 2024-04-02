c289f70538db09680957b77390e8759ef5c6a524
Trevor Harada <trevor.harada2015@gmail.com>
2024-04-02 05:12:19 -0500

    .

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+let a = 1;
+let b =1;
+if (n < 0) {
+    return "OOPS";
+} else {
+    for (let i = 3; i <= n; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+};
 };
 
 // Do not edit below this line
